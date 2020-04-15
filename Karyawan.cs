a1897727e43496157a1db95d2db84e10c2a1c7b6
Fery Apriliawan <fery.apriliawan@students.amikom.ac.id>
2020-04-15 15:20:19 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Responsi
+{
+    public class Karyawan
+    {
+        public Karyawan(string name, int no, int nik, int gaji)
+        {
+            Nik = nik;
+            No = no;
+            Name = name;
+            Gaji = gaji;
+        }
+
+        public string Name { get; set; }
+        public int Gaji { get; set; }
+        public int Nik { get; set; }
+        public int No { get; set; }
+
+        public void gaji()
+        {
+            Console.WriteLine("Karyawan has name : {o} no : {1} nik : {2} gaji {3}", Nama No, Nik, Gaji);
+        }
+        public void gajinaik()
+        {
+            Console.WriteLine("Karyawan has name : {o} no : {1} nik : {2} gaji {3}", Nama No, Nik, Gaji + (Gaji * 10 / 100));
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Responsi
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Karyawan Karyawan = new Karyawan("190302123 Budi", 3000000);
+            Karyawan.GetNameAndAge();
+
+            Karyawan karyawan = new Karyawan("190302124 Budi", 2000000);
+            karyawan.GetNameAndAge();
+        }
+    }
+}
